329f351bbc9cc2ab7e4d586d4d885b37298a9feb
Candace Camarillo <ccamarillo@ccamarillo.local>
2021-10-10 14:10:46 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// document.addEventListener("DOMContentLoaded",function(){
+//     var slidingMenu = function() {
+//         var menuToggle = document.querySelector('.menu-toggle');
+//         var body       = document.body;
+
+//         menuToggle.addEventListener('click', function(ev) {
+//             ev.preventDefault();
+//             body.classList.toggle('menu-active');
+//         });
+//     }
+//     slidingMenu();
+
+//     var closeButton = function() { 
+//         var closeButton = document.querySelector("[rel=close-button]");
+//         var body = document.body;
+
+//         closeButton.addEventListener('click', function(ev) {
+//             ev.preventDefault();
+//             body.classList.toggle('menu-active');
+//         });
+//     }
+//     closeButton();
+// });
---
+++ js/menu.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const element = <h1>Hello, world</h1>;
+    ReactDOM.render(element, document.getElementById('nav'));
+});
